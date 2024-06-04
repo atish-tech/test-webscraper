56c6c80d007ea30c5c673f8a2702517076e48361
aatishthakur <atishthakur@truedax.io>
2024-06-04 15:15:12 +0530

    change host

--- utils/ApiRoutes.ts
+++ utils/ApiRoutes.ts
@@ -1,3 +1,4 @@
-export const host = "http://localhost:3000";
+// export const host = "http://localhost:3000";
+export const host = "https://test-webscraper.vercel.app";
 
-export const webScraperRoutes = `${host}/api/webscraper`
+export const webScraperRoutes = `${host}/api/webscraper`;
