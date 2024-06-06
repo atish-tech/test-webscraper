e39199ec5970521e3327e825f008a60689e5629a
aatishthakur <atishthakur@truedax.io>
2024-06-06 08:18:34 +0530

    ..

--- utils/ApiRoutes.ts
+++ utils/ApiRoutes.ts
@@ -1,4 +1,5 @@
-export const host = "http://192.168.1.4:8000";
+// export const host = "http://192.168.1.4:8000";
 // export const host = "https://test-webscraper.vercel.app";
+export const host = "https://test-webscraper.onrender.com";
 
 export const webScraperRoutes = `${host}/api/webscraper`;
